cf514ac6fa6dc33c33c9fec1bb83f100b2443ce3
kage <13613514381@163.com>
2019-08-16 20:15:28 +0800

    init repo

---
+++ src/main/java/com/kage/stu/ComKageStuApplication.java
@@ -0,0 +1,13 @@
+package com.kage.stu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComKageStuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ComKageStuApplication.class, args);
+    }
+
+}
